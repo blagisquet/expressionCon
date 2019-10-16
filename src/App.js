8dee93f50adcdd79d2aae5444f537369b5e2755e
blagisquet <baptiste.lagisquet@gmail.com>
2019-10-16 19:28:59 +0200

    first init and save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [expressions, setExpressions] = useState([]);
+  const API = '../api/sentences.json';
+
+  useEffect(() => {
+    axios.get(API)
+    .then((result) => {
+      setExpressions(result.data);
+    })
+    // .then(expressions => {
+    //   let sentence = expressions[Math.floor(Math.random() * expressions.sentences)];
+    // })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="container">
+        <section className="open-book">
+          <header>
+            <h1>Expressions à la con</h1>
+            <h6>par Tristan et Baptiste</h6>
+          </header>
+          <article>
+            {expressions.map((expression, index) => {
+              return (
+                <p>{expression}</p>
+              )
+            })}
+          </article>
+          <footer>
+            <ol id="page-numbers">
+              <li>1</li>
+              <li>2</li>
+            </ol>
+          </footer>
+        </section>
+      </div>
     </div>
   );
 }
